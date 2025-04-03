33490083c47c45387d1014360198f1185f7c9f29
SyaDo95 <70837945+SyaDo95@users.noreply.github.com>
2025-04-03 18:52:46 +0900

    스프링부트 시작

---
+++ spring_boot/scope-api/src/main/java/com/scope/backend/scope_api/ScopeApiApplication.java
@@ -0,0 +1,13 @@
+package com.scope.backend.scope_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopeApiApplication.class, args);
+	}
+
+}
